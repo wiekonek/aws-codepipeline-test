e04fae9f360370747f230fe6e83f77230e5e9d0b
Dariusz Dwornikowski <dariusz.dwornikowski@cs.put.poznan.pl>
2018-01-04 16:36:02 +0100

    Tests

---
+++ app.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import flask
+
+def string_ccat(a,b):
+    return a+b
+
+print("ola")
---
+++ test_func.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import unittest
+from app import string_ccat
+
+class Testing(unittest.TestCase):
+
+    def test_concat(self):
+        self.assertEqual(string_ccat("a", "b"), "ab")
